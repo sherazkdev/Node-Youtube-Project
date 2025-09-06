5385013ac127b6bf8ba647721e810460c4ccc724
Sheraz Khan <sameturmux@gmail.com>
2025-09-06 13:36:37 +0500

    auth middleware error is cleared and add envorments variables

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use( express.json() );
 app.use( express.urlencoded( { extended : true } ) );
 app.use( cookieParser() );
 app.use(cors({
-    origin: "http://localhost:5173", // React app origin
+    origin: process?.env?.CORS_ORIGIN, // React app origin
     credentials: true
   }));
 
